787e9a92c5fa10fd800f7849d61975bafeda3b1f
Fernando Nassar <fernandonassar14@yahoo.com.br>
2021-07-01 22:08:18 -0300

    feign client

--- hr-payroll/src/main/java/com/demo/project/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/com/demo/project/hrpayroll/services/PaymentService.java
@@ -1,13 +1,34 @@
 package com.demo.project.hrpayroll.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.demo.project.hrpayroll.entities.Payment;
+import com.demo.project.hrpayroll.entities.Worker;
+import com.demo.project.hrpayroll.feignclients.WorkerFeignClient;
 
 @Service
 public class PaymentService {
 	
+	@Autowired
+	private WorkerFeignClient workerFeignClient;
+	
 	public Payment getPayment(Long workerId, Integer days) {
-		return new Payment("Bob", 200.0, days);
+		
+		Worker worker = workerFeignClient.findById(workerId).getBody();
+		
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
